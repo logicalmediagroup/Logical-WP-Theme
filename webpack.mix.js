f4f221af6f06a4fd2a8b9eafbebf2706f4156bbd
MARLMG <152422770+MAR-GUEZ@users.noreply.github.com>
2024-08-09 22:09:31 -0500

    Fixed issue with webpack where the menu would not work. PurgeCSS has been disabled

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,41 +1,44 @@
 // webpack.mix.js
 
-let mix = require('laravel-mix');
-require('laravel-mix-purgecss');
+let mix = require("laravel-mix");
+/*require("laravel-mix-purgecss");*/
 
 // Config
 
 mix.webpackConfig({
-    stats: {
-        children: true
-    }
+  stats: {
+    children: true,
+  },
 });
 
 // CSS
 
-mix.
-    sass('assets/styles/style.scss', 'style.css')
-    .options({
-        processCssUrls: false
-    })
-    .purgeCss({
-        content: [
-            '*.php',
-        ],
-        safelist: {
-            standard: [
-                /^text-/,
-                /^bg-/,
-                /^visible-/,
-                /^hidden-/,
-                /^btn/,
-            ]
-        }
-    });
+mix.sass("assets/styles/style.scss", "style.css").options({
+  processCssUrls: true,
+});
+/*.purgeCss({
+    content: ["*.php"], // Ensure all relevant files are included
+    safelist: {
+      standard: [
+        /^text-/,
+        /^bg-/,
+        /^visible-/,
+        /^hidden-/,
+        /^btn/,
+        "nav",
+        "nav--header",
+        "li",
+        "ul",
+        "js-menu-toggle",
+        "nav-burger",
+        "navigation",
+        "header_navigation",
+        // Add more classes here as needed
+      ],
+    },
+    rejected: true, // Enable rejected option to see what is being removed
+  });*/
 
 // JS
 
-mix
-    .js([          
-        'assets/scripts/scripts.js'
-    ], 'js/scripts.min.js');
+mix.js(["assets/scripts/scripts.js"], "js/scripts.min.js");
